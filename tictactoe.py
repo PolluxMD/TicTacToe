5e21ae97429f59b3c33c41eaf0fa4e83ec77c8aa
vitorisalves <vitorisalves1@gmail.com>
2025-05-28 20:04:22 -0300

    adicionando função utility e minimax

--- tictactoe.py
+++ tictactoe.py
@@ -54,19 +54,20 @@ def winner(board):
     """
     Returns the winner of the game, if there is one.
     """
-    #Verifica linhas e colunas
+    # Verifica linhas e colunas
     for i in range(3):
-        if board[i][0] == board[i][1] == board[i][2] == board[i][0] is not EMPTY:
-            return board[0][i]
-        if board[0][i] == board[1][i] == board[2][i] == board[0][i] is not EMPTY:
+        if board[i][0] == board[i][1] == board[i][2] != EMPTY:
             return board[i][0]
-    #Verifica Diagonais
-        if board[0][0] == board[1][1] == board[2][2] == board[0][0] is not EMPTY:
-            return board[0][j]
-        if board[0][2] == board[1][1] == board[2][0] == board[0][2] is not EMPTY:
-            return board[0][2]
-
-        return None
+        if board[0][i] == board[1][i] == board[2][i] != EMPTY:
+            return board[0][i]
+    
+    # Verifica diagonais
+    if board[0][0] == board[1][1] == board[2][2] != EMPTY:
+        return board[0][0]
+    if board[0][2] == board[1][1] == board[2][0] != EMPTY:
+        return board[0][2]
+    
+    return None
 
 
 def terminal(board):
@@ -86,11 +87,56 @@ def utility(board):
     """
     Returns 1 if X has won the game, -1 if O has won, 0 otherwise.
     """
-    raise NotImplementedError
+    win = winner(board)
+    if win == X:
+        return 1
+    elif win == O:
+        return -1
+    else:
+        return 0
+
 
 
 def minimax(board):
     """
     Returns the optimal action for the current player on the board.
     """
-    raise NotImplementedError
+    if terminal(board):
+        return None
+
+    turn = player(board)
+
+    def max_value(state):
+        if terminal(state):
+            return utility(state), None
+        v = -math.inf
+        best_move = None
+        for action in actions(state):
+            min_v, _ = min_value(result(state, action))
+            if min_v > v:
+                v = min_v
+                best_move = action
+                if v == 1:
+                    break  # melhor valor possível para X
+        return v, best_move
+
+    def min_value(state):
+        if terminal(state):
+            return utility(state), None
+        v = math.inf
+        best_move = None
+        for action in actions(state):
+            max_v, _ = max_value(result(state, action))
+            if max_v < v:
+                v = max_v
+                best_move = action
+                if v == -1:
+                    break  # melhor valor possível para O
+        return v, best_move
+
+    if turn == X:
+        _, action = max_value(board)
+    else:
+        _, action = min_value(board)
+    return action
+
